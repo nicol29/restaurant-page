78424089814b05a5a2235b89a443c539364c90d7
nicol29 <nicolascondrea@gmail.com>
2023-01-09 18:41:24 +0000

    Add gitignore file

--- src/about.js
+++ src/about.js
@@ -3,10 +3,14 @@ import { createFooter } from "./home";
 const loadAboutPage = () => {
   let contentContainer = document.querySelector('#content');
 
-  
+
   let footerSection = createFooter();
 
   contentContainer.append(footerSection);
+}
+
+const createBody = () => {
+  let bodyContainer = {}
 }
 
 export default loadAboutPage;
